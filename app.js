ac9f19bf614333f3a0aba06b88cddb5d22542121
unknown <you@example.com>
2022-08-16 20:03:35 +0400

    its work

--- app.js
+++ app.js
@@ -54,3 +54,100 @@ let changeColor = ()=>{
 
 
 setInterval(changeColor,2000)
+
+
+
+//տնային աշխատանք 2 կալկուլյատորի ստեղծում
+
+document.write ( "<br>");
+document.write ( "<br>");
+let input1 = document.createElement('input');
+document.body.append(input1);
+input1.id = "input1"
+input1.value = "enter a number like - 1234"
+document.write ( "<br>");
+document.write ( "<br>");
+let input2 = document.createElement('input');
+document.body.append(input2);
+input2.id = "input2"
+input2.value = "enter only operators + - * /"
+document.write ( "<br>");
+document.write ( "<br>");
+let input3 = document.createElement('input');
+document.body.append(input3);
+input3.id = "input3"
+input3.value = "enter a number like - 1234"
+document.write ( "<br>");
+document.write ( "<br>");
+let button = document.createElement('button');
+document.body.append(button);
+button.id = "submit";
+button.innerHTML = "=";
+document.write ( "<br>");
+let p = document.createElement('p')
+document.body.append(p);
+p.id = "p";
+
+document.getElementById('submit').onclick = func = () =>{
+   
+	let operator = input2.value;
+	let num1 = input1.value;
+	let num2 = input3.value;
+ 
+
+		let result;
+	if(!(Number(num1)) || !(Number(num2))){
+		alert('please enter a number')
+	}else if(operator === '+'){
+        button.textContent = 'Processing…'
+		result = (Number(num1) + Number(num2))
+	}else if(operator === '-'){
+        button.textContent = 'Processing…'
+	    result = (Number(num1) - Number(num2))
+	}else if(operator === '*'){
+        button.textContent = 'Processing…'
+	    result = (Number(num1) * Number(num2))
+	}else if(operator === '/'){
+        button.textContent = 'Processing…'
+	    result = (Number(num1) / Number(num2))
+	}else{
+		alert('not the same operator');
+	}
+	
+	p.innerHTML = `the result of calculaiting is ${result}`;
+};
+
+
+
+//dvd tnain ashxatank 
+
+let elem = document.getElementById('animation_div');
+let positionYMax = (window.innerHeight - elem.clientHeight) + 'px';
+let positionXMax = (window.innerWidth - elem.clientWidth) + 'px';
+let positionXMin = '0px';
+let positionYMin = '0px';
+let reverse_y = true;
+let reverse_x = true;
+
+let myFunc = () => {
+    if(reverse_x) {
+        elem.style.left =`${elem.offsetLeft + 100}px`;
+    } else {
+        elem.style.left =`${elem.offsetLeft - 100}px`;
+    }
+    if(reverse_y) {
+        elem.style.top =`${elem.offsetTop + 100}px`;
+    } else {
+        elem.style.top =`${elem.offsetTop - 100}px`;
+    }
+
+    if(elem.style.top > positionYMax || elem.style.top === positionYMin) {
+        reverse_y = !reverse_y;
+    }
+    if(elem.style.left > positionXMax || elem.style.left === positionXMin) {
+        reverse_x = !reverse_x;
+    }
+
+}
+
+setInterval(myFunc,500);
